ced2fccca6fc375c1a255d4ad0da039fc054a7cd
Matthew Hosking <hosking.m@husky.neu.edu>
2017-12-11 15:28:47 -0500

    added some javadoc stuff

--- src/cs3500/animator/view/graphics/TickScrubberSlider.java
+++ src/cs3500/animator/view/graphics/TickScrubberSlider.java
@@ -5,9 +5,16 @@ import cs3500.animator.control.IAnimationController;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Represents a slider meant to scrub through an animation's ticks.
+ */
 public class TickScrubberSlider extends JSlider {
     private IAnimationController controller;
 
+    /**
+     * Creates a new {@code TickScrubberSlider} object.
+     * @param controller is the controller linked to this slider.
+     */
     public TickScrubberSlider(IAnimationController controller) {
         super(1, controller.getLastTick());
         this.controller = controller;
